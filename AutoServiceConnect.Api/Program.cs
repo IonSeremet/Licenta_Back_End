f5f7df02e2f9d37f3f5994d20a4d34b667fded9f
Seremet Valeriu <valeriu3333@gmail.com>
2024-07-26 13:59:54 +0300

    Add swagger authorization

--- AutoServiceConnect.Api/Program.cs
+++ AutoServiceConnect.Api/Program.cs
@@ -5,6 +5,7 @@ using AutoServiceConnect.Api.Middlewares;
 using AutoServiceConnect.Api.Services;
 using AutoServiceConnect.Api.Utils;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers().AddJsonOptions(x =>
@@ -21,7 +22,36 @@ var appSettings = new AppSettings();
 builder.Configuration.Bind(appSettings);
 builder.Services.AddSingleton(appSettings);
 
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Auto Service Management", Version = "v1" }); 
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header,
+
+            },
+            new List<string>()
+        }
+    });
+});
 builder.Services.AddDbContext<AutoServiceDbContext>(options =>
     options.UseSqlServer(appSettings.SqlConnectionString));
 builder.Services.AddScoped<UserService>();
